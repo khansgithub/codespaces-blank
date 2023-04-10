e9d87843ad8cc5ee85f01ba80c626f39129377b3
khan <Khan.Ahmed@city.ac.uk>
2023-04-10 23:46:42 +0000

    update

--- clone_repo.py
+++ clone_repo.py
@@ -1,4 +1,4 @@
-import subprocess, os
+import subprocess, os, pexpect
 
 def subprocess_run(cmd, **kwargs):
     return subprocess.run(
@@ -8,14 +8,53 @@ def subprocess_run(cmd, **kwargs):
         **kwargs
     ).stdout.decode("utf-8")
 
-folder_name = "mod"
-if not os.path.isdir(os.path.join(os.getcwd(), folder_name)):
-    raise Exception(f"'{folder_name}' does not exist in {os.getcwd()}")
+
+def check_known_hosts():
+    set_known_hosts = True
+    try:
+        # Replace this with `subprocess_run`
+        keyscan = subprocess_run(
+            "ssh-keygen -H -F main_db", check=True, stdout=subprocess.PIPE
+        ).stdout.decode("utf-8")
+        if len(keyscan) > 1:
+            set_known_hosts = False
+            pass
+    except Exception as err:
+        pass
+
+    if set_known_hosts:
+        cmds = [
+            "mkdir -p /root/.ssh",
+            "touch /root/.ssh/known_hosts",
+            f"ssh-keyscan {HOST}"
+        ]
+        process = None
+        for c in cmds:
+            try:
+                # Replace this with `subprocess_run`
+                process = subprocess_run(c, check=True, stdout=subprocess.PIPE)
+            except subprocess.CalledProcessError as err:
+                pass
+
+        print(process)
+        with open("/root/.ssh/known_hosts", 'a') as known_hosts:
+            known_hosts.write(process.stdout.decode("utf-8"))
+            known_hosts.write("\n")
 
 
 HOST = subprocess_run("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' git")
+REPO = "repo"
 PORT = 3333
-PATH = "/clone_dir/"
-cmd = f"git clone --branch=master ssh://git@{HOST}:{PORT}/srv/git/{folder_name} {PATH}"
+CLONE_TO = "./mod/"
 
-subprocess_run(cmd)
+if not os.path.isdir(os.path.join(os.getcwd(), CLONE_TO)):
+    raise Exception(f"'{CLONE_TO}' does not exist in {os.getcwd()}")
+check_known_hosts()
+
+cmd = f"git clone --branch=master ssh://git@{HOST}:{PORT}/srv/{REPO} {CLONE_TO}"
+child = pexpect.spawn(cmd, timeout=10)
+child.expect("Cloning(.*)")
+child.expect("git(.*)")
+child.sendline('12345')
+child.expect("Resolving deltas: 100%(.*)")
+child.read()
--- git_container/up.sh
+++ git_container/up.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+PORT=3333
 docker run \
 	-d \
 	--name git \
 	-v "$(pwd)/"entrypoint.sh:/usr/home/entrypoint.sh \
     -v "$(pwd)/"repo:/srv/repo \
 	--entrypoint "/usr/home/entrypoint.sh" \
-	-p 2220:22 \
-    --rm \
+	--rm \
+	-p $PORT:22 \
      --health-cmd "set -e;  nc -zv localhost 22; if [ $? -eq 0 ]; then exit 0; else exit 1; fi" \
      --health-interval=10s \
 	alpine
 
-cd repo
-git init
-touch foo
-git add foo
-git config --local user.name "foo"
-git config --local user.email "foo@bar.com"
-git commit -m "initial commit"
-cd ~
-mkdir clone_dir
-sudo ln -s ~/clone_dir /clone_dir
+# cd repo
+# git init
+# touch foo
+# git add foo
+# git config --local user.name "foo"
+# git config --local user.email "foo@bar.com"
+# git commit -m "initial commit"
+# cd ~
+# mkdir clone_dir
+
+if [ ! -L /clone_dir ]; then
+	sudo ln -s /home/codespace/codespaces-blank/mod /clone_dir
+fi
 
 # Wait for the container to become healthy
 while true; do
     HEALTH=$(docker inspect --format='{{.State.Health.Status}}' git)
     if [ "$HEALTH" == "healthy" ]; then
-        ssh-keyscan -p 2222 localhost >> ~/.ssh/known_hosts
+        ssh-keyscan -p $PORT localhost >> ~/.ssh/known_hosts
         break
     else
         true
